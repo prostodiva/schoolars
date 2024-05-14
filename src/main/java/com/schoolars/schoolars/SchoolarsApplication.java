1c92dc26b58d8e8d1a2c962791388de1ebc51fde
prostodiva <rkattsyna@mail.ru>
2024-05-13 20:24:10 -0700

    First version

---
+++ src/main/java/com/schoolars/schoolars/SchoolarsApplication.java
@@ -0,0 +1,13 @@
+package com.schoolars.schoolars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolarsApplication.class, args);
+	}
+
+}
