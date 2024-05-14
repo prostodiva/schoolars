3acfbf9755cefbbc8c50689d0c47ea0bafe57388
prostodiva <rkattsyna@mail.ru>
2024-05-13 20:36:03 -0700

    Added .sql script /db/migrations/V1_DDL

---
+++ target/classes/db/migrations/V1_DDL.sql
@@ -0,0 +1,21 @@
+CREATE TABLE clubs (
+club_id SERIAL PRIMARY KEY,
+club_name VARCHAR(255) NOT NULL,
+cabinet_member VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+user_id SERIAL PRIMARY KEY,
+user_firstName VARCHAR(255) NOT NULL,
+user_lastName VARCHAR(255) NOT NULL,
+user_email VARCHAR(255) NOT NULL,
+user_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE announcements (
+announcements_id SERIAL PRIMARY KEY,
+user_id int,
+FOREIGN KEY (user_id) REFERENCES users (user_id),
+club_id int,
+FOREIGN KEY (club_id) REFERENCES clubs (club_id)
+);
