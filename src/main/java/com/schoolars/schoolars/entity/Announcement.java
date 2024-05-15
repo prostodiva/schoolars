ca4f495850c3959bb178ddd1d55ea3045d7d2b32
prostodiva <rkattsyna@mail.ru>
2024-05-15 13:34:08 -0700

    announcements entity created

---
+++ src/main/java/com/schoolars/schoolars/entity/Announcement.java
@@ -0,0 +1,9 @@
+package com.schoolars.schoolars.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "announcements")
+public class Announcement {
+    
+}
